e03907d4a9372c03b740de4f65d43f4059231bb1
HolyCow <mike.beasterfeld@gmail.com>
2013-10-02 17:39:45 -0500

    Don't allow commands from IRC

--- lib/shewbot.rb
+++ lib/shewbot.rb
@@ -25,11 +25,6 @@ class TimedEvents
   timer 30, :method => :send_timer_message
 
   def send_timer_message
-    puts 'Checking twitter'
-
-    puts ENV['TWITTER_REGEX']
-
-    puts last_tweet
 
     if last_tweet =~ /#{ENV['TWITTER_REGEX']}/
       new_show = Regexp.last_match[1]
@@ -39,6 +34,8 @@ class TimedEvents
       if currentshow && new_show == Show.current.title
         return
       end
+
+      Show.create(:title => new_show, :updated_at => Time.new)
 
       if ENV['EMAIL_ON_NEW_SHOW'] == 1
 
@@ -72,13 +69,15 @@ class TimedEvents
         end
       end
 
+      puts ENV['DELETE_ON_NEW_SHOW']
+
       if ENV['DELETE_ON_NEW_SHOW'] == 1
-        Vote.all.destroy
-        Title.all.destroy
-        Show.all.destroy
+        puts "Deleting all records"
+        currentshow.votes.destroy
+        currentshow.titles.destroy
+        currentshow.destroy
       end
 
-      Show.create(:title => new_show, :updated_at => Time.new)
     end
 
   end
@@ -93,57 +92,6 @@ bot = Cinch::Bot.new do
     c.channels = [ENV['BOTCHANNEL']]
     c.nick = ENV['BOTNAME']
     c.plugins.plugins = [TimedEvents]
-  end
-
-  on :message, /@5by5: (.*?) with .*? is starting now/ do |m, show|
-
-    if m.user.nick.downcase == 'showbot' || m.user.nick.downcase == '_holycow'
-      emailbody = ''
-
-      puts "---------------"
-      puts "Email on new show = " + ENV['EMAIL_ON_NEW_SHOW']
-      puts "---------------"
-
-      if ENV['EMAIL_ON_NEW_SHOW']
-
-        options = { :address              => ENV['EMAIL_SERVER'],
-                    :port                 => ENV['EMAIL_PORT'],
-                    :domain               => ENV['EMAIL_DOMAIN'],
-                    :user_name            => ENV['EMAIL_USER'],
-                    :password             => ENV['EMAIL_PASSWORD'],
-                    :authentication       => ENV['EMAIL_AUTH'],
-                    :enable_starttls_auto => true  }
-
-        puts options
-
-        Mail.defaults do
-          delivery_method :smtp, options
-        end
-
-        Title.all.each do | t |
-          emailbody << "#{t.title}, #{t.user}, #{t.votes}\n"
-        end
-
-        currentshow = Show.current
-
-        if currentshow
-          Mail.deliver do
-                 to ENV['EMAIL_TO']
-               from ENV['EMAIL_USER']
-            subject "Titles for #{currentshow.title}"
-               body emailbody
-          end
-        end
-      end
-
-      if ENV['DELETE_ON_NEW_SHOW']
-        Title.all.destroy
-        Show.all.destroy
-        Vote.all.destroy
-      end
-
-      Show.create(:title => show, :updated_at => m.time)
-    end
   end
 
   on :message, /^!s (.*$)/ do |m, title|
@@ -164,7 +112,7 @@ bot = Cinch::Bot.new do
   end
 
   on :message, /^!help/ do |m|
-    m.user.send "!s - suggest a title; !last - I'll tell you what last tweet by @#{ENV['TWITTER_USER']}; !help - this"
+    m.user.send "!s - suggest a title; !last - I'll tell you the last tweet by @#{ENV['TWITTER_USER']}; !help - this"
   end
 
   on :message, /^!last/ do |m|
